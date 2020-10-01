0448abd19d74b933a9edbb9e07e89edd7a5b9762
Habel Justin <habeljustin48@gmail.com>
2020-10-01 17:06:06 +0800

    fix buildfield access key

--- app/src/main/java/com/example/unsplashimagesearch/api/UnSplashApi.kt
+++ app/src/main/java/com/example/unsplashimagesearch/api/UnSplashApi.kt
@@ -1,21 +1,23 @@
 package com.example.unsplashimagesearch.api
 
+
 import com.example.unsplashimagesearch.BuildConfig
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Query
 
+
 interface UnSplashApi {
 
     companion object {
         const val BASE_URL = "https://api.unsplash.com/"
-        const val CLIENT_ID = BuildConfig.UNSPLASH_ACCESS_KEY
+        const val UNSPLASH_ACCESS_KEY = BuildConfig.UNSPLASH_ACCESS_KEY
     }
 
     @Headers(
         "Accept-Version: v1",
-        "Authorization: Client-ID $CLIENT_ID"
+        "Authorization: Client-ID $UNSPLASH_ACCESS_KEY"
     )
     @GET("search/photos")
     suspend fun searchPhoto(
