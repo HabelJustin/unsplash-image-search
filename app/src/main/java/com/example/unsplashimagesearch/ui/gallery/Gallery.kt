83f68dcb30d3a3867a1e283f35130941cbbe31c5
Habel Justin <habeljustin48@gmail.com>
2020-10-05 16:50:43 +0800

    adapter addLoadListener

--- app/src/main/java/com/example/unsplashimagesearch/ui/gallery/Gallery.kt
+++ app/src/main/java/com/example/unsplashimagesearch/ui/gallery/Gallery.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.util.Log
 import android.view.*
 import androidx.appcompat.widget.SearchView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.paging.LoadState
 import com.example.unsplashimagesearch.R
 import com.example.unsplashimagesearch.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -35,10 +37,29 @@ class Gallery : Fragment(R.layout.fragment_gallery) {
                 header = UnSplashPhotoLoadStateAdapter(retryAdapter),
                 footer = UnSplashPhotoLoadStateAdapter(retryAdapter),
             )
+            buttonRetry.setOnClickListener {
+                retryAdapter.invoke()
+            }
         }
 
         viewModel.photos.observe(viewLifecycleOwner) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
+        }
+
+        adapter.addLoadStateListener { loadState ->
+            binding.apply {
+                progressBar.isVisible = loadState.source.refresh is LoadState.Loading
+                recyclerView.isVisible = loadState.source.refresh is LoadState.NotLoading
+                buttonRetry.isVisible = loadState.source.refresh is LoadState.Error
+                textViewError.isVisible = loadState.source.refresh is LoadState.Error
+
+                if(loadState.source.refresh is LoadState.NotLoading && loadState.append.endOfPaginationReached && adapter.itemCount < 1){
+                    recyclerView.isVisible = false
+                    textViewEmpty.isVisible = true
+                }else{
+                    textViewEmpty.isVisible = false
+                }
+            }
         }
 
         setHasOptionsMenu(true)
